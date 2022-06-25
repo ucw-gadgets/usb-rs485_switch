20e80374109d32ff55db0d885d68956b1ebc5ff1
Martin Mares <mj@ucw.cz>
2022-06-25 22:34:40 +0200

    Firmware: Init

---
+++ sw/firmware/config.h
@@ -0,0 +1,14 @@
+/*
+ *	USB-RS485 Switch -- Configuration
+ *
+ *	(c) 2022 Martin Mareš <mj@ucw.cz>
+ */
+
+// Processor clock
+
+#define CPU_CLOCK_MHZ 72
+
+// Debugging port
+
+#define DEBUG_USART USART2
+#define DEBUG_LED_BLUEPILL
---
+++ sw/firmware/interface.h
@@ -0,0 +1,10 @@
+/*
+ *	USB-RS485 Switch -- Interface Definitions
+ *
+ *	(c) 2022 Martin Mareš <mj@ucw.cz>
+ */
+
+#define USB_RS485_USB_VENDOR 0x4242
+#define USB_RS485_USB_PRODUCT 0x000b
+#define USB_RS485_USB_VERSION 0x0100
+
